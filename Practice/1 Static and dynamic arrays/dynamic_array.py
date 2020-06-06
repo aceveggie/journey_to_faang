33b98ad9ebfb0047a4369662beb4fc3e84a0cfb8
rahul <1053406+aceveggie@users.noreply.github.com>
2020-06-06 10:36:50 -0700

    separate variables for checking max and current size

--- Practice/1 Static and dynamic arrays/dynamic_array.py
+++ Practice/1 Static and dynamic arrays/dynamic_array.py
@@ -6,6 +6,7 @@ My implementation of dynamic array
 class DynArray:
     def __init__(self, max_size=2):
         self.max_size = max_size
+        self.cur_size = max_size
         self.array = [None]*self.max_size
 
     def insert(self, value, index):
@@ -15,15 +16,18 @@ class DynArray:
         # if exceeds, double the size
         # copy the values appropriately
         # insert the values
+        # update current size
         pass
 
     def remove(self, value):
         # remove the value
+        # udpate current size
         pass
 
     def append(self):
         # check if appending will exceed size
         # if not, insert at end
+        # udpate current size
         pass
 
     def get_max_size(self):
@@ -36,6 +40,7 @@ class DynArray:
     def __change_size(self):
         # lets create a new array with double the max_size of current array
         # copy over the elements
+        # udpate current size
         pass
 
 
