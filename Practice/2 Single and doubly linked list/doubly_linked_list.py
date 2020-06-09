2935b7b15d9e8ecb473a1e336a5569af3a8e0419
rahul <kavirahul085@gmail.com>
2020-06-08 22:21:38 -0700

    basic implementation complete. need to handle corner cases

--- Practice/2 Single and doubly linked list/doubly_linked_list.py
+++ Practice/2 Single and doubly linked list/doubly_linked_list.py
@@ -11,22 +11,30 @@ class Node:
         self.next = None
         self.prev = None
         self.data = data
+    
+    def __str__(self):
+        return str(self.data)
+    
+    def __repr__(self):
+        return str(self.data)
 
 
 class DoubleList:
 
-    def __init__(self):
-        self._length = 0
-        self.head = None
-        self.tail = None
+    def __init__(self, data_val):
+        new_node = Node(data_val)
+        self.head = new_node
+        self.tail = new_node
+        self._length = 1
 
     def print_all(self):
         if self._length <= 0:
             return
         cur_node = self.head
-        while cur_node.data is not None:
+        while cur_node is not None:
             print(cur_node.data, end=' ')
             cur_node = cur_node.next
+        print()
 
     def clear(self):
         if self._length <= 0:
@@ -48,61 +56,120 @@ class DoubleList:
     def _add_first(self, data_val):
         cur_head = self.head
         new_node = Node(data_val)
-        pass
+        cur_head.prev = new_node
+        new_node.next = cur_head
+        self.head = new_node
 
     def _add_last(self, data_val):
         cur_tail = self.tail
-        pass
+        new_node = Node(data_val)
+        cur_tail.next = new_node
+        new_node.prev = cur_tail
+        self.tail = new_node
 
     def append(self, data_val):
-        pass
+        self._add_last(data_val)
+        self._length += 1
 
-    def insert_at(self, index, data_val):
-        pass
+    def insert(self, index, data_val):
+        if index == 0:
+            self._add_first(data_val)
+        elif index == self._length:
+            self._add_last(data_val)
+        else:
+            idd = 0
+            prev_node = None
+            cur_node = self.head
+            while idd != index:
+                prev_node = cur_node
+                cur_node = cur_node.next
+                idd += 1
+            new_node = Node(data_val)
+            prev_node.next = new_node
+            new_node.prev = prev_node
+            new_node.next = cur_node
+        self._length += 1
 
     def _remove_first(self):
-        pass
+        cur_head = self.head
+        if self._length <= 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = cur_head.next
+            self.head.prev = None
 
     def _remove_last(self):
-        pass
+        cur_tail = self.tail
+        if self._length <= 1:
+            self.tail = None
+            self.head = None
+        else:
+            self.tail = cur_tail.prev
+            self.tail.next = None
 
-    def remove_at(self, index):
-        pass
+    def remove(self, index):
+        print('removing node at index', index)
+        if index == 0:
+            self._remove_first()
+        elif index == self._length - 1:
+            self._remove_last()
+        else:
+            idd = 0
+            prev_node = None
+            cur_node = self.head
+            while idd != index:
+                prev_node = cur_node
+                cur_node = cur_node.next
+                idd += 1
+            prev_node.next = cur_node.next
+            cur_node.next.prev = prev_node
+        self._length -= 1
 
     def get_length(self, index):
-        pass
+        self._length
 
     def get_first(self):
         return self.head.data
 
     def get_last(self):
         return self.tail.data
+    
+    def _debug(self):
+        cur_node = self.head
+        print('---')
+        while cur_node is not None:
+            print('cur node: ', cur_node.prev,'<-->',cur_node.data, '<-->', cur_node.next)
+            cur_node = cur_node.next
+        print('--------------------')
 
 if __name__ == "__main__":
     print('done')
     list_obj = DoubleList(10)
+    list_obj._debug()
     list_obj.append(20)
+    list_obj._debug()
     list_obj.append(30)
+    list_obj._debug()
     list_obj.append(40)
-    list_obj.print_all()
+    list_obj._debug()
     # list_obj.clear()
     list_obj.insert(4, 5)
-    list_obj.insert(4, 50)
-    list_obj.print_all()
-    print(list_obj.get_first())
-    print(list_obj.get_last())
-    list_obj.print_all()
+    list_obj._debug()
+    list_obj.insert(5, 50)
+    list_obj._debug()
+    #list_obj._debug()
     list_obj.remove(1)
-    list_obj.print_all()
+    # list_obj.print_all()
+    list_obj._debug()
     list_obj.remove(1)
-    list_obj.print_all()
-    list_obj.remove(1)
-    list_obj.print_all()
-    list_obj.remove(1)
-    list_obj.print_all()
-    list_obj.remove(1)
-    list_obj.print_all()
+    # list_obj.print_all()
+    list_obj._debug()
     # list_obj.remove(1)
-    list_obj.print_all()
-    print(list_obj.get_first())
-    print(list_obj.get_last())
+    # list_obj.print_all()
+    # #list_obj._debug()
+    # list_obj.remove(1)
+    # list_obj.print_all()
+    # #list_obj._debug()
+    # print(list_obj.get_first())
+    # print(list_obj.get_last())
