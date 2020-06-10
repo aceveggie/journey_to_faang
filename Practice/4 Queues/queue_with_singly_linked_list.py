22ef2b6fbec3996c183d4f06129619cc3922f18e
rahul <1053406+aceveggie@users.noreply.github.com>
2020-06-10 08:28:51 -0700

    implemented simple queue using singly linkedlist

---
+++ Practice/4 Queues/queue_with_singly_linked_list.py
@@ -0,0 +1,99 @@
+'''
+implement a queue with singly linked list
+'''
+
+
+class Node:
+    '''
+        Simple node
+    '''
+    def __init__(self, data_val):
+        self.data = data_val
+        self.next = None
+
+    def __str__(self):
+        return str(self.data)
+
+    def __repr__(self):
+        return str(self.data)
+
+
+class SinglyLinkedListQueue:
+    '''
+        implements singly linked list queue
+        enqueue adds at tail
+        deque removes from head
+    '''
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+        self.pointer = self.head
+
+    def _add_first(self, data_val):
+        '''
+            to be called when size = 0
+        '''
+        new_node = Node(data_val)
+        self.head = new_node
+        self.tail = new_node
+
+    def enque(self, data_val):
+        '''
+        insert new node at tail
+        update tail
+        '''
+        if self.size < 1:
+            self._add_first(data_val)
+        else:
+            cur_tail = self.tail
+            new_node = Node(data_val)
+            cur_tail.next = new_node
+            self.tail = new_node
+        self.size += 1
+
+    def deque(self):
+        if self.size <= 1:
+            self.head = None
+            self.tail = None
+            self.size = 0
+            return
+        else:
+            ret_node = self.head
+            self.head = self.head.next
+            self.size -= 1
+            return ret_node
+
+    def get_size(self):
+        return self.size
+
+    def get_first(self):
+        return self.head
+
+    def get_last(self):
+        return self.tail
+
+    def print_all(self):
+        cur_node = self.head
+        while cur_node is not None:
+            print(cur_node, end=' ')
+            cur_node = cur_node.next
+        print()
+
+
+if __name__ == "__main__":
+    q_obj = SinglyLinkedListQueue()
+    q_obj.enque(10)
+    q_obj.enque(20)
+    q_obj.enque(30)
+    q_obj.enque(40)
+    q_obj.enque(50)
+    q_obj.print_all()
+    print(q_obj.deque())
+    q_obj.print_all()
+    print(q_obj.deque())
+    print(q_obj.deque())
+    print(q_obj.deque())
+    print(q_obj.deque())
+    print('done all opeartions:')
+    q_obj.print_all()
